f433981def8c9816f8d5b15b374410c25262b2fa
José Aleixo <josealeixo.pc@gmail.com>
2017-10-06 17:11:19 +0100

    Added Levels and Agents packages.

---
+++ Agents-Pack-29-04-2016/Source Code/SubGoalAStarAgents/Node.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace GeometryFriendsAgents
+{
+    class Node
+    {
+        private int x = 0;
+        private int y = 0;
+        private bool diamond = false;
+        private bool leadsToFallDown = false;
+        private bool pseudo = false;
+
+        public Node(int x, int y, bool diamond)
+        {
+            this.x = x;
+            this.y = y;
+            this.diamond = diamond;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            // Check for null values and compare run-time types.
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Node n = (Node)obj;
+            return (x == n.x) && (y == n.y) && (diamond == n.diamond);
+        }
+
+        public override String ToString()
+        {
+            return "X: " + x + " Y: " + y + " Diamond: " + diamond;
+        }
+
+        public int getX()
+        {
+            return this.x;
+        }
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public int getY()
+        {
+            return this.y;
+        }
+
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+
+        public bool getDiamond()
+        {
+            return this.diamond;
+        }
+
+        public void setDiamond(bool diamond)
+        {
+            this.diamond = diamond;
+        }
+
+        public bool getLeadsToFallDown()
+        {
+            return leadsToFallDown;
+        }
+
+        public void setLeadsToFallDown(bool fallDown)
+        {
+            this.leadsToFallDown = fallDown;
+        }
+
+        public bool getPseudo()
+        {
+            return pseudo;
+        }
+
+        public void setPseudo(bool pseudo)
+        {
+            this.pseudo = pseudo;
+        }
+
+    }
+}
